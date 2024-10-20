efc5443f5121908819645b8f6f9629d203cbb7b9
powerofcode2023 <holbertonc21@gmail.com>
2024-10-20 11:50:54 -0400

    task 9

--- linux_security/0x03_protocols_servers/9-firewall.sh
+++ linux_security/0x03_protocols_servers/9-firewall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-iptables -P INPUT DROP
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -P INPUT -j DROP 
+iptables -A INPUT -p tcp --dport ssh -j ACCEPT &&  iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
